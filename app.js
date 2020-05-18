c4bda48191ebaae179c1175b882e14167e28aa63
Richard Oden <richard.thomas.oden@gmail.com>
2020-05-18 18:15:24 -0400

    Change map elements from divs to table

--- app.js
+++ app.js
@@ -10,18 +10,20 @@ function print(message) {
     mapContainer.innerHTML += message;
 }
 
-for (let x = 0; x < 20; x++) {
+for (let x = 0; x < 21; x++) {
     let mapArrY = []
-    for (let y = 0; y < 20; y++) {
+    for (let y = 0; y < 21; y++) {
         mapArrY[y] = getRandTerrain();
     }
     mapArrX[x] = mapArrY;
 }
 
 for (let x = 0; x < mapArrX.length; x++) {
+    let tr = document.createElement('tr');
+    mapContainer.appendChild(tr);
     for (let y = 0; y < mapArrX[x].length; y++) {
-        let div = document.createElement("div");
-        div.style.backgroundColor = mapArrX[x][y];
-        mapContainer.appendChild(div);
+        let td = document.createElement("td");
+        td.style.backgroundColor = mapArrX[x][y];
+        tr.appendChild(td);
     }
 }
