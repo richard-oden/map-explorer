4095f1a022876fb8a85fdc4572feb6d1604b72ae
Richard Oden <richard.thomas.oden@gmail.com>
2020-05-19 18:33:30 -0400

    Reduce reflow violation

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mapContainer = document.getElementById('map');
+const root = document.documentElement;
 let mapArrX = [];
 const terrain = ['#B0ED38', '#41B30C', '#EED869', '#CFCCBC', '#66B2EF'];
 
@@ -23,6 +24,9 @@ for (let x = 0; x < 21; x++) {
     mapArrX[x] = mapArrY;
 }
 
+// Create CSS variable to assign cell size relative to number of rows:
+root.style.setProperty('--grid-cell-size', mapContainer.offsetHeight / mapArrX.length + 'px');
+
 // Draw terrain:
 for (let x = 0; x < mapArrX.length; x++) {
     let tr = document.createElement('tr');
@@ -30,7 +34,6 @@ for (let x = 0; x < mapArrX.length; x++) {
     for (let y = 0; y < mapArrX[x].length; y++) {
         let td = document.createElement("td");
         td.style.backgroundColor = mapArrX[x][y];
-        td.style.height = mapContainer.offsetHeight / mapArrX.length + 'px';
         // Draw player character if td is in center of map:
         if (x === getCenter(mapArrX) && y === getCenter(mapArrX[x])) {
             td.setAttribute("id", "player");
