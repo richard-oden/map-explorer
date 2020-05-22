4e5523affb2a74fcd4c944427a5f257d45efd9fd
Richard Oden <richard.thomas.oden@gmail.com>
2020-05-22 10:46:36 -0400

    Attempt to generate larger chunks of terrain (getting multiple undefined values in adjTerrain array)

--- app.js
+++ app.js
@@ -31,19 +31,20 @@ function drawMap() {
             if (x === getCenter(mapArrX) && y === getCenter(mapArrX[x])) {
                 html += ` id="player"><img src="img/player.png" alt="player">`;
             }
-            // Plains:
+            // Draw locations based on terrain:
+            // Plains
             else if (mapArrX[x][y] === terrain[0]) {
                 if (roll(15)) html += ` class="snake"><img src="img/snake.png" alt="snake">`;
-            // Forest:
+            // Forest
             } else if (mapArrX[x][y] === terrain[1]) {
                 if (roll(15)) html += ` class="bear"><img src="img/bear.png" alt="bear">`;
-            // Desert:
+            // Desert
             } else if (mapArrX[x][y] === terrain[2]) {
                 if (roll(15)) html += ` class="scorpion"><img src="img/scorpion.png" alt="scorpion">`;
-            // Mountain:
+            // Mountain
             } else if (mapArrX[x][y] === terrain[3]) {
 
-            // Water:
+            // Water
             } else if (mapArrX[x][y] === terrain[4]) {
                 if (roll(15)) html += ` class="shark"><img src="img/shark.png" alt="shark">`;
             } 
@@ -58,23 +59,35 @@ function drawMap() {
 for (let x = 0; x < 21; x++) {
     let mapArrY = []
     for (let y = 0; y < 21; y++) {
-        // Randomly copy adjacent cell to create larger patches of terrain:
-        // let adjTerrain = [];
-        // if (roll(5) && y - 1 > -1) adjTerrain.push(mapArr[x][y - 1]);
-        // if (roll(5) && x - 1 > -1) adjTerrain.push(mapArr[x - 1][y]);
-        // if (roll(5) && (x - 1 > -1 && y - 1 > -1)) adjTerrain.push(mapArr[x - 1][y - 1]);
-        // if (roll(5) && (x - 1 > -1 && y < mapArrX[x].length)) adjTerrain.push(mapArr[x - 1][y + 1]);
-        // if (roll(5) && y + 1 < mapArrX[x].length) adjTerrain.push(mapArr[x][y + 1]);
-        // if (roll(5) && x + 1 < mapArrX.length) adjTerrain.push(mapArr[x + 1][y]);
-        // if (roll(5) && (x + 1 < mapArrX.length && y - 1 > -1)) adjTerrain.push(mapArr[x + 1][y - 1]);
-        // if (roll(5) && (x + 1 < mapArrX.length && y < mapArrX[x].length)) adjTerrain.push(mapArr[x + 1][y + 1]);
-
-        // if (roll(2)) {
-        //     mapArrY[y] = adjTerrain[Math.floor(Math.random() * adjTerrain.length)];
-        // } else {
-        //     mapArrY[y] = getRandTerrain();
-        // }
         mapArrY[y] = getRandTerrain();
+    }
+    mapArrX[x] = mapArrY;
+}
+
+// Create larger chunks of terrain:
+// Randomly copy adjacent cell (50%) unless it is first element in mapArrX or mapArrY:
+for (let x = 0; x < 21; x++) {
+    let mapArrY = []
+    for (let y = 0; y < 21; y++) {
+        if ( roll(2) && (y > 0 && x > 0) ) {
+            let adjTerrain = [];
+            adjTerrain.push(mapArrX[x][y - 1]); 
+            adjTerrain.push(mapArrX[x - 1][y]); 
+            adjTerrain.push(mapArrX[x - 1][y - 1]);
+            if (y + 1 < mapArrX[x].length) {
+                adjTerrain.push(mapArrX[x][y + 1]); 
+                adjTerrain.push(mapArrX[x - 1][y + 1]);
+            }
+            if (x + 1 < mapArrX.length) {
+                adjTerrain.push(mapArrX[x + 1][y]);
+                adjTerrain.push(mapArrX[x + 1][y - 1]);
+            }
+            if (x + 1 < mapArrX.length && y + 1 < mapArrX[x].length) {
+                adjTerrain.push(mapArrX[x + 1][y + 1]);
+            }
+            console.log(adjTerrain);
+            mapArrY[y] = adjTerrain[Math.floor(Math.random() * adjTerrain.length)];
+        }
     }
     mapArrX[x] = mapArrY;
 }
